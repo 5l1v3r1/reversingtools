1a5c07ce007e4fe366e9f93cb61b7755fe944dd0
jgeigerm <jmg2967@rit.edu>
2014-06-11 19:47:09 -0400

    Added byte count output to stderr

--- x86/comp_and_dump.sh
+++ x86/comp_and_dump.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+c=0
 nasm -f elf32 -o shl.o shl.asm && ld -s -m elf_i386 -o shl shl.o
-for i in $(objdump -d shl | tail -n +8 | cut -f2);do echo -n '\\x'$i;done
+rm -f shl.o
+for i in $(objdump -d shl | tail -n +8 | cut -f2);do echo -n '\\x'$i;c=$((c+1));done
+echo
+echo "$c Bytes" >&2
